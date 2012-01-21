e416a022c7e7f9e6e7f367eb172f6f02cd70b15b
sparkgene <jun1ka0@gmail.com>
2012-01-21 09:24:57 +0900

    Initial Commit

---
+++ recorder_test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  recorder_test
+//
+//  Created by 純 市川 on 12/01/21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
